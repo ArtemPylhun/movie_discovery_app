a6966e196800ad96a92da949b75be5fa31463359
Artem Pylhun <artem.pylhun@honeycombsoft.com>
2025-09-23 22:29:31 +0300

    Fix test class name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:shimmer/main.dart';
+import 'package:movie_discovery_app/main.dart';
 
 void main() {
   testWidgets('App starts without crashing', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MainApp());
 
     // Verify that the app starts
     expect(find.byType(MaterialApp), findsOneWidget);
